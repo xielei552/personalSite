cd9f3e81b1fc713c7f20f8acbc54ea0eac711236
xielei552 <xielei552@gmail.com>
2017-04-06 23:06:24 -0400

    First Commit

---
+++ creat-signature/signature.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<?php
+$name=$_POST["Name"];
+echo $name;
+?>
+
+Welcome <?php echo $_POST["Name"]; ?><br>
+Your email address is: <?php echo $_POST["Email"]; ?>
+<?php
+echo "<h2>this is what you have created $name";
+
+
+?>
+
+</body>
+</html>
